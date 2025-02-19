821824aaf482a71b0e6fd8683c930c765d85ecb7
Michael Kalish <michael.kalish@focusconsulting.io>
2025-02-18 19:14:24 -0500

    DMN Rules Task Work Item

---
+++ perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/wih/DMNRulesTaskWorkItemHandler.java
@@ -0,0 +1,21 @@
+package com.focus.irs.pv.prototype.wih;
+
+import org.kie.kogito.internal.process.runtime.KogitoWorkItem;
+import org.kie.kogito.internal.process.runtime.KogitoWorkItemHandler;
+import org.kie.kogito.internal.process.runtime.KogitoWorkItemManager;
+
+public class DMNRulesTaskWorkItemHandler implements KogitoWorkItemHandler {
+
+    @Override
+    public void executeWorkItem(KogitoWorkItem workItem, KogitoWorkItemManager manager) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'executeWorkItem'");
+    }
+
+    @Override
+    public void abortWorkItem(KogitoWorkItem workItem, KogitoWorkItemManager manager) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'abortWorkItem'");
+    }
+
+}
