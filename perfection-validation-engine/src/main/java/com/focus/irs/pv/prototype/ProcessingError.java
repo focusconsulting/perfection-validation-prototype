597edcdecc2a82d1b6695aafae2744001520572f
Michael Kalish <michael.kalish@focusconsulting.io>
2025-03-03 08:03:51 -0500

    fixup! Add initial input/output models

--- perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/ProcessingError.java
+++ perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/ProcessingError.java
@@ -7,6 +7,14 @@ public class ProcessingError<T> {
     private Optional<T> value;
     private ErrorCode errorCode;
 
+    public Optional<T> getValue() {
+        return value;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+
     public ProcessingError(Optional<T> value, ErrorCode errorCode) {
         this.value = value;
         this.errorCode = errorCode;
