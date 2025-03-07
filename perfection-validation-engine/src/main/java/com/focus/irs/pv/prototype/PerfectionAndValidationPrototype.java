a9fcc16d0fb8e055140b5756d448e4138d96fb1f
Michael Kalish <michael.kalish@focusconsulting.io>
2025-03-07 13:36:12 -0500

    Update spring scanned paths

--- perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/PerfectionAndValidationPrototype.java
+++ perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/PerfectionAndValidationPrototype.java
@@ -3,7 +3,7 @@ package com.focus.irs.pv.prototype;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = { "org.kie.kogito.**", "http**", "com.focus.irs.**", })
+@SpringBootApplication(scanBasePackages = { "org.kie.kogito.**", "http**", "com.focus.irs.**", "pv**" })
 public class PerfectionAndValidationPrototype {
     public static void main(String[] args) {
         SpringApplication.run(PerfectionAndValidationPrototype.class, args);
