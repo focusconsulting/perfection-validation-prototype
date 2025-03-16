1ee7996b69cd71affefc5d4e86086cc1cca361bd
Your Name <you@example.com>
2025-03-16 16:19:52 -0400

    feat: Add Form943Data class for prototype validation engine

---
+++ perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/Form943Data.java
@@ -0,0 +1,51 @@
+package com.focus.irs.pv.prototype;
+
+public class Form943Data {
+    // AI! update this class so that it is a POJO representation of the following XML
+    /**
+     * 
+     * <Form943>
+  <TaxYear>2024</TaxYear>
+  <FilingDate>2025-01-15</FilingDate>
+  <EmployerInfo>
+    <Name>Agricultural Enterprises Inc</Name>
+    <EIN>12-3456789</EIN>
+    <Address>...</Address>
+  </EmployerInfo>
+  <EmploymentData>
+    <HasAgriculturalEmployees>true</HasAgriculturalEmployees>
+    <PaidWagesOver150>true</PaidWagesOver150>
+    <EmployedContinuousLaborer>true</EmployedContinuousLaborer>
+  </EmploymentData>
+  <TaxData>
+    <TotalWages>250000.00</TotalWages>
+    <FederalIncomeTaxWithheld>35000.00</FederalIncomeTaxWithheld>
+    <EmployerSocialSecurityTax>15500.00</EmployerSocialSecurityTax>
+    <EmployeeSocialSecurityTax>15500.00</EmployeeSocialSecurityTax>
+    <MedicareTax>7250.00</MedicareTax>
+    <AdditionalMedicareTax>500.00</AdditionalMedicareTax>
+  </TaxData>
+  <Adjustments>
+    <FractionOfCentAdjustment>0.25</FractionOfCentAdjustment>
+    <SickPayAdjustment>-150.00</SickPayAdjustment>
+    <OtherAdjustments>0.00</OtherAdjustments>
+  </Adjustments>
+  <Credits>
+    <HasResearchCredit>true</HasResearchCredit>
+    <CreditAmount>2500.00</CreditAmount>
+    <Form8974Attached>true</Form8974Attached>
+  </Credits>
+  <Deposits>
+    <TotalDeposits>71100.25</TotalDeposits>
+    <DepositsComplete>true</DepositsComplete>
+    <DepositDates>
+      <Date>2024-04-15</Date>
+      <Date>2024-07-15</Date>
+      <Date>2024-10-15</Date>
+      <Date>2025-01-15</Date>
+    </DepositDates>
+  </Deposits>
+</Form943>
+     */
+
+}
