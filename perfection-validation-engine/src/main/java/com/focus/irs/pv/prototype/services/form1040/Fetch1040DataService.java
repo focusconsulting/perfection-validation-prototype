b8160d7763668920b3e70273fef8851fe1a276f4
Your Name <you@example.com>
2025-03-20 09:01:46 -0400

    feat: Add Fetch1040DataService for retrieving form 1040 data

---
+++ perfection-validation-engine/src/main/java/com/focus/irs/pv/prototype/services/form1040/Fetch1040DataService.java
@@ -0,0 +1,15 @@
+package com.focus.irs.pv.prototype.services.form1040;
+
+import java.util.Arrays;
+
+import com.focus.irs.pv.prototype.FilingStatus;
+import com.focus.irs.pv.prototype.Form1040Data;
+import com.focus.irs.pv.prototype.credits.dependents.DependentInformation;
+import com.focus.irs.pv.prototype.messages.Process1040Message;
+
+public class Fetch1040DataService {
+
+    Form1040Data fetchData(Process1040Message message ) {
+        
+    }
+}
